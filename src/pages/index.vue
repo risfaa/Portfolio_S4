a12b317d0d565885dccedca82200c5def0b5b44d
risfaa <faris.halepovic@edu.univ-fcomte.fr>
2024-03-22 21:26:15 +0100

    home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,30 @@
 </script>
 
 <template>
-  <div>
-    <h1>Home</h1>
+  <div class="home-page">
+    <img src="/img/home_page/Portfolio.png" alt="" class="home_page--img">
   </div>
 </template>
+
+<style>
+.home-page {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-image: url('/img/home_page/DESIGNSYNDROME-PAPERCUT-TEXTURE.png');
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.home_page--img {
+  width: 75%;
+}
+@media (min-width: 768px) {
+  .home_page--img {
+    width: 40%;
+    max-width: 550px;
+  }
+}
+</style>
