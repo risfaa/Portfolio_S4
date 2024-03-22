1ca22656241baaf7334db659a2a2d04ea52b09da
risfaa <faris.halepovic@edu.univ-fcomte.fr>
2024-03-22 19:50:39 +0100

    add color var

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'noir': '#1e1e1e',
+      'bleu': '#2575a8',
+      'bleuFonce': '#487089',
+      'blanc': '#fafafa',
+    },
     extend: {},
   },
   plugins: [],
