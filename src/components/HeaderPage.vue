fc588e62f1e818b6a7674a52e2cd2fe2fa3a63b1
risfaa <faris.halepovic@edu.univ-fcomte.fr>
2024-03-22 21:26:05 +0100

    header v1

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -4,6 +4,11 @@
 
 <template>
   <header>
-    <h1>Header</h1>
+    <nav class="border__header">
+      <ul class="flex justify-center gap-8 sm:gap-12 mt-3">
+        <li><RouterLink to="/projets">Projets</RouterLink></li>
+        <li><RouterLink to="/about">A propos</RouterLink></li>
+      </ul>
+    </nav>
   </header>
 </template>
