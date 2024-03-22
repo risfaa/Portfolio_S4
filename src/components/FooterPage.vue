0a8adfcedc0edba5a1e6d0ba9414314806c5c75c
risfaa <faris.halepovic@edu.univ-fcomte.fr>
2024-03-22 21:25:51 +0100

    footer not visible index

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
+const showFooter = ref(true)
+
+if (route.path === '/') {
+  showFooter.value = false
+} else {
+  showFooter.value = true
+}
 </script>
 
 <template>
-  <footer>
+  <footer v-if="showFooter">
     <h1>Footer</h1>
   </footer>
 </template>
