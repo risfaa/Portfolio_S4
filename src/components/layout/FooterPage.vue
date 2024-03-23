9b8e311d5a36f8759cc5027897cab9126259f1fc
risfaa <faris.halepovic@edu.univ-fcomte.fr>
2024-03-23 22:22:36 +0100

    add cv pdf

--- src/components/layout/FooterPage.vue
+++ src/components/layout/FooterPage.vue
@@ -20,7 +20,7 @@ watch(route, () => {
 <template>
   <footer v-if="showFooter" class="bg-noir text-blanc text-center p-10 sm:p-20">
     <h1 class="font-Heavy text-2xl sm:text-3xl">Faris HALEPOVIC</h1>
-    <a href="" download="" class="text-bleu text-lg sm:text-xl">Télécharger mon CV</a>
+    <a href="/file/CV_Faris_HALEPOVIC.pdf" download="CV_Faris_HALEPOVIC.pdf" class="text-bleu text-lg sm:text-xl hover:underline">Télécharger mon CV</a>
     <ul class="text-xl sm:text-2xl flex justify-center gap-5 mt-5">
       <li><RouterLink to="/projets">Projets</RouterLink></li>
       <li><RouterLink to="/about">A propos</RouterLink></li>
