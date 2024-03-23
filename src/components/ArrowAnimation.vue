fac6b33b851d58a26bb92499bdf107382a92481d
risfaa <faris.halepovic@edu.univ-fcomte.fr>
2024-03-23 15:30:14 +0100

    animation slide v1

---
+++ src/components/ArrowAnimation.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Arrow from '@/components/icons/Arrow.vue';
+
+</script>
+
+<template>
+    <div class="carousel-container">
+        <div class="carousel-wrapper">  
+            <div v-for="i in 30" class="carousel-items">
+                <Arrow class="max-sm:w-5" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.carousel-container {
+  overflow: hidden;
+  width: 11vh;
+}
+
+.carousel-wrapper {
+  display: flex;
+  gap: 0.5rem;
+  animation: slide 30s linear infinite;
+}
+
+@keyframes slide {
+  from {
+    transform: translateX(-550%);
+  }
+  to {
+    transform: translateX(0%);
+  }
+}
+</style>
