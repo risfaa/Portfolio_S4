a54618d0e0b76806382439de1ca92219f1e4d711
risfaa <faris.halepovic@edu.univ-fcomte.fr>
2024-03-24 00:04:29 +0100

    create template project page

---
+++ src/components/ProjectPage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import HeroProjet from '../components/HeroProjet.vue';
+
+defineProps<{
+    imgPathHero: string;
+    imgPathCover: string;
+    titre: string;
+    textBtn: string;
+    link: string;
+}>();
+</script>
+<template>
+    <div class="mt-[12vh] md:px-[10%]">
+        <HeroProjet :imgPath="imgPathHero" :titre="titre" :textBtn="textBtn" :link="link" />
+    </div>
+    <div>
+        <img :src="imgPathCover" alt="Img couverture">
+        <slot name="desription"></slot>
+    </div>
+</template>
--- src/pages/CouvBleach.vue
+++ src/pages/CouvBleach.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import HeroProjet from '../components/HeroProjet.vue';
+import ProjectPage from '../components/ProjectPage.vue';
 
 </script>
 <template>
-    <div class="mt-[12vh] md:px-[10%]">
-        <HeroProjet imgPath="img/home_page/DESIGNSYNDROME-PAPERCUT-TEXTURE.png" titre="Couvertures Hunter X Hunter dans le style Bleach" textBtn="Retour" link="/" />
-    </div>
+    <ProjectPage imgPathHero="img/home_page/DESIGNSYNDROME-PAPERCUT-TEXTURE.png" titre="Couvertures Hunter X Hunter dans le style Bleach" textBtn="Retour" link="/">
+        <slot name="desription"></slot>
+    </ProjectPage>
 </template>
