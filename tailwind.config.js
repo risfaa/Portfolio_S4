26adc3b349c08e429d074c617b7b9a4ff1da1387
risfaa <faris.halepovic@edu.univ-fcomte.fr>
2024-03-23 13:40:04 +0100

    add font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,18 @@ export default {
   theme: {
     colors: {
       'noir': '#1e1e1e',
-      'bleu': '#2575a8',
+      'bleu': '#2575A8',
       'bleuFonce': '#487089',
       'blanc': '#fafafa',
+    },
+    fontSize: {
+      'sm': '.875rem', // 14px
+      'base': '1rem', // 16px
+      'lg': '1.125rem', // 18px
+      'xl': '1.25rem', // 20px
+      '2xl': '1.5rem', // 24px
+      '3xl': '1.875rem', // 30px
+      '4xl': '2.5rem', // 40px
     },
     extend: {},
   },
