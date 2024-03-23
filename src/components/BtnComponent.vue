680fada36e62a5778dc765e5b1686858bebb1ec1
risfaa <faris.halepovic@edu.univ-fcomte.fr>
2024-03-23 22:23:17 +0100

    create btn & hero component

---
+++ src/components/BtnComponent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+defineProps<{
+    text: String,
+    link: String
+}>()
+</script>
+<template>
+    <RouterLink :to="link">
+        <button class="bg-noir text-blanc text-sm sm:text-base p-1 sm:p-2 px-2 sm:px-3 rounded-lg font-Bold">
+            <span>{{ text }}</span>
+        </button>
+    </RouterLink>
+</template>
---
+++ src/components/HeroProjet.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import BtnComponent from '../components/BtnComponent.vue';
+
+defineProps<{
+    imgPath: String,
+    titre: String,
+    textBtn: String,
+    link: String
+}>()
+</script>
+<template>
+    <div>
+        <div class="relative">
+            <BtnComponent class="absolute top-3 left-3 sm:top-8 sm:left-8" :text="textBtn" :link="link" />
+            <img :src="imgPath" alt="Image couverture" class="rounded-lg">
+        </div>
+        <h2 class="font-Bold text-2xl md:text-5xl max-w-[900px] leading-tight mt-4">{{ titre }}</h2>
+    </div>
+</template>
