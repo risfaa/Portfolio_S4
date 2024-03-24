741a90e82d4ed73dba28ceacf24cdc6f43b8f3b4
risfaa <faris.halepovic@edu.univ-fcomte.fr>
2024-03-24 16:01:57 +0100

    update

--- src/components/ProjectPage.vue
+++ src/components/ProjectPage.vue
@@ -7,14 +7,27 @@ defineProps<{
     titre: string;
     textBtn: string;
     link: string;
+    titreFresque?: string;
 }>();
 </script>
 <template>
     <div class="mt-[12vh] md:px-[10%]">
         <HeroProjet :imgPath="imgPathHero" :titre="titre" :textBtn="textBtn" :link="link" />
     </div>
+    <div class="grid__projects my-10 lg:my-40 md:px-10 lg:gap-20 xl:px-[20%]">
+        <img :src="imgPathCover" alt="Img couverture" class="rounded-lg">
+        <div class="font-Roman grid h-full justify-between gap-4 lg:py-10">
+            <slot name="description"></slot>
+        </div>
+    </div>
     <div>
-        <img :src="imgPathCover" alt="Img couverture">
-        <slot name="desription"></slot>
+        <h3 class="mx-auto leading-tight max-w-[950px] font-Bold sm:text-center text-lg sm:text-3xl sm:mt-20 mb-8 md:mb-20">{{ titreFresque }}</h3>
+        <slot name="fresque"></slot>
+    </div>
+    <div class="sm:px-10 lg:px-[15%] py-5 lg:py-40">
+        <p class="max-w-[900px] font-Bold leading-tight text-base lg:text-2xl mt-14 sm:mt-32 sm:pr-[20%] lg:pr-0 mb-8 sm:mb-20">Vous pouvez découvrir toutes les couvertures dans leurs entièreté avec compris la 1er et 4ème de couverture ainsi que la tranche ci dessous.</p>
+        <div class="grid grid-cols-3 gap-2 sm:gap-4 lg:gap-8 transition-all duration-300">
+            <slot name="couvertures"></slot>
+        </div>
     </div>
 </template>
--- src/pages/CouvBleach.vue
+++ src/pages/CouvBleach.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
 import ProjectPage from '../components/ProjectPage.vue';
+import FresqueComponent from '../components/FresqueComponent.vue';
+import FullCouvComponent from '../components/FullCouvComponent.vue';
 
 </script>
 <template>
-    <ProjectPage imgPathHero="img/home_page/DESIGNSYNDROME-PAPERCUT-TEXTURE.png" titre="Couvertures Hunter X Hunter dans le style Bleach" textBtn="Retour" link="/">
-        <slot name="desription"></slot>
+    <ProjectPage 
+        imgPathHero="img/home_page/DESIGNSYNDROME-PAPERCUT-TEXTURE.png" 
+        titre="Couvertures Hunter X Hunter dans le style Bleach" 
+        textBtn="Retour" 
+        link="/"
+        imgPathCover="/img/Photo_Faris.png"
+        titreFresque="Voici les couvertures que j’ai réaliser pour le manga Hunter X Hunter dans le style des couvertures du manga Bleach."
+    >
+        <template #description>
+            <p>Il y 3 ans une trend est apparue sur les réseaux sociaux qui consiste à reproduire le style des couvertures du manga “bleach” de l’auteur <em>TITE Kubo</em> et de les appliquer sur les couverture d’autres mangas.</p>
+            <p>C’est en découvrant cette trend et un artiste français <em>dendenmushiproduction</em> sur instagram qui lui aussi de son coté fait également des couvertures personnalisée du manga One piece dans le style des couverutres du manga Bleach ; que j’ai décider de me lancer moi aussi dans la confection de personnalisation de couverture de mangas dans le style de “Bleach”.</p>
+        </template>
+        <template #fresque>
+            <FresqueComponent 
+                img1="/img/Photo_Faris.png"
+                img2="/img/Photo_Faris.png"
+                img3="/img/Photo_Faris.png"
+                img4="/img/Photo_Faris.png"
+                img5="/img/Photo_Faris.png"
+                img6="/img/Photo_Faris.png"
+                img7="/img/Photo_Faris.png"
+                img8="/img/Photo_Faris.png"
+                img9="/img/Photo_Faris.png"
+                img10="/img/Photo_Faris.png"
+                img11="/img/Photo_Faris.png"
+                img12="/img/Photo_Faris.png"
+                imgCouv="/img/Photo_Faris.png"
+            />
+        </template>
+        <template #couvertures>
+            <FullCouvComponent imgPath="/img/Photo_Faris.png" imgPathFull="img/home_page/DESIGNSYNDROME-PAPERCUT-TEXTURE.png" />
+
+        </template>
     </ProjectPage>
-</template>
+</template> 
